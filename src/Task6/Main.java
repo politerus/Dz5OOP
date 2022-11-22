74e4a48381ee3daded4dc12108fd5ce33be6bb74
Roman <romkagm1992@gmail.com>
2022-11-22 19:47:26 +0200

    Dz5OOP

--- src/Task6/Main.java
+++ src/Task6/Main.java
@@ -1,3 +1,5 @@
+package Task6;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,9 +13,9 @@ public class Main {
         teachers.add("Людмила Степановна");
         teachers.add("Ольга Ивановна");
         teachers.add("Лариса Сергеевна");
-       Collections.sort(teachers);
-       String [] array = new String[teachers.size()];
-       teachers.toArray(array);
+        Collections.sort(teachers);
+        String [] array = new String[teachers.size()];
+        teachers.toArray(array);
 
         System.out.println("Мои учителя "+teachers);
         System.out.println("Индекс лучшего учителя : "+ Arrays.binarySearch(array,"Людмила Степановна"));
